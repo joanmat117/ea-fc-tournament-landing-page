46b27b42647bb137eab8fc3a14af1164fa0d8ee9
Yoan <yoan16908@gmail.com>
2025-12-19 18:05:01 -0500

    basic header added

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-export async function getTranslation(lang:'es'|'en'|'fr'|string){
+export async function getTranslation(lang:string|undefined){
   const l = lang !== 'es' && lang !== 'en' && lang !== 'fr' ? 'es' : lang
   const t = await import(`../i18n/${l}.json`)
   return t
